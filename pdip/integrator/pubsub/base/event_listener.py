7821aa12b1ec3306bbab952d0edd3da6221b8eda
Ahmet Çağrı AKCA <ahmetcagriakca@gmail.com>
2022-01-03 13:49:23 +0300

    operation and integration initialize

--- pdip/integrator/pubsub/base/event_listener.py
+++ pdip/integrator/pubsub/base/event_listener.py
@@ -1,6 +1,7 @@
 import queue
 import threading
 
+from pdip.logging.loggers.console import ConsoleLogger
 from .channel_queue import ChannelQueue
 from ..domain import TaskMessage
 
@@ -9,19 +10,18 @@ class EventListener(threading.Thread):
     def __init__(self,
                  channel: ChannelQueue,
                  subscribers: {},
+                 logger:ConsoleLogger,
                  *args, **kwargs
                  ):
+        self.logger = logger
         self.subscribers = subscribers
         self.channel = channel
         super().__init__(*args, **kwargs)
 
-    # def __iter__(self):
     def run(self):
         while True:
             try:
                 task: TaskMessage = self.channel.get()
-                # print(f'subscriber message:{task.message}')
-
                 if task.event in self.subscribers.keys():
                     for callback in self.subscribers[task.event]:
                         callback(**task.kwargs)
