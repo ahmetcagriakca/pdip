2c391d693f45091f806c8ef99c5340bf656c9fab
Ahmet Çağrı AKCA <ahmetcagriakca@gmail.com>
2022-01-05 04:39:37 +0300

    update message broker

--- pdip/integrator/pubsub/base/message_broker_worker.py
+++ pdip/integrator/pubsub/base/message_broker_worker.py
@@ -11,20 +11,32 @@ class MessageBrokerWorker(threading.Thread):
                  message_channel: ChannelQueue,
                  other_arg,
                  *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.message_channel = message_channel
         self.publish_channel = publish_channel
         self.other_arg = other_arg
-        super().__init__(*args, **kwargs)
+        self._stop_event = threading.Event()
+
+    def stop(self):
+        self._stop_event.set()
+
+    def stopped(self):
+        return self._stop_event.is_set()
 
     def run(self):
         while True:
             try:
                 work: TaskMessage = self.publish_channel.get()
+                if work is None:
+                    return
                 self.message_channel.put(work)
                 if work.is_finished:
                     break
                 self.publish_channel.done()
             except queue.Empty:
+                return
+            except Exception as e:
+                self.logger.error('error')
                 return
             finally:
                 pass
--- pdip/integrator/pubsub/domain/message.py
+++ pdip/integrator/pubsub/domain/message.py
@@ -1,42 +1,9 @@
-import queue
-import threading
-
-from .channel_queue import ChannelQueue
-from ..domain import TaskMessage
+from dataclasses import dataclass
 
 
-class MessageBrokerWorker(threading.Thread):
-    def __init__(self,
-                 publish_channel: ChannelQueue,
-                 message_channel: ChannelQueue,
-                 other_arg,
-                 *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.message_channel = message_channel
-        self.publish_channel = publish_channel
-        self.other_arg = other_arg
-        self._stop_event = threading.Event()
-
-    def stop(self):
-        self._stop_event.set()
-
-    def stopped(self):
-        return self._stop_event.is_set()
-
-    def run(self):
-        while True:
-            try:
-                work: TaskMessage = self.publish_channel.get()
-                if work is None:
-                    return
-                self.message_channel.put(work)
-                if work.is_finished:
-                    break
-                self.publish_channel.done()
-            except queue.Empty:
-                return
-            except Exception as e:
-                self.logger.error('error')
-                return
-            finally:
-                pass
+@dataclass
+class TaskMessage:
+    event: any = None
+    is_finished: bool = None
+    args: () = None
+    kwargs: {} = None
